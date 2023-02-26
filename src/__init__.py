30d1826ba97be9d26e791547222ec0da2ac93663
joebaillie21 <jjb7866@g.rit.edu>
2023-02-26 03:08:50 -0500

    totally didnt fix your quiz or search prompt nah

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,5 @@
-from src.quiz import take_quiz
+from quiz import take_quiz
+import little_guy_finder
 
 
 def print_file(filename):
@@ -8,26 +9,44 @@ def print_file(filename):
     f.close()
 
 
-def main():
-    print_file('data/name.txt')
-    print_file('data/emotional_support.txt')
-
-    bad_val = True
-    response = None
-    print('Welcome to Lil Guy finder. Please choose an option:')
+def quiz_or_search():
+    print('Welcome to Lil Guy finder.')
     print('1. Take the quiz')
     print('2. Search for a lil guy')
     print('3. Exit')
-    while (bad_val):
-        response = input()
-        if (response != '1' and response != '2' and response != '3'):
-            print('Please input one of the given values.')
-        else:
-            bad_val = False
-
+    response = input('Please enter \'1\', \'2\', or \'3\': ')
     match response:
         case '1':
-            take_quiz()
+            return response
+        case '2':
+            return response
+        case '3':
+            return response
+        case _:
+            return quiz_or_search()
+
+
+def print_lil_guy(lil_guy):
+    print('Name:', lil_guy[0])
+    print('Description:', lil_guy[1])
+    if (lil_guy[2]):
+        print('Friend Shaped.')
+    print('Traits:')
+    for x in range(3, len(lil_guy)):
+        if lil_guy[x] != None:
+            print('\t' + lil_guy[x])
+
+
+def main():
+    print_file('data/name.txt')
+    print()
+    print_file('data/emotional_support.txt')
+    print()
+
+    match quiz_or_search():
+        case '1':
+            print()
+            print_lil_guy(little_guy_finder.get_lil_guy_by_name(take_quiz()))
         case '2':
             """ search() """
         case '3':
